55c5b42656550458415bdcb64570811ec0474a6d
ali.raza5 <ali.raza@convo.com>
2022-10-08 16:25:36 +0500

    Add 'dist/' from commit '1f0b25c848921a54a56c5ede32bff34c2095a4e1'
    git-subtree-dir: dist
    git-subtree-mainline: 1f0b25c848921a54a56c5ede32bff34c2095a4e1
    git-subtree-split: 1f0b25c848921a54a56c5ede32bff34c2095a4e1

---
+++ dist/repo/src/_mocks/data.js
@@ -0,0 +1,92 @@
+const data = {
+  // General
+  name: "Ali Raza",
+  phone: "+92 340 5202204",
+  email: "alirazaunir401@gmail.com",
+  dob: "06 July 1999",
+  short_bio: "I’m Javascript Engineer.",
+  bio: "Javascript Engineer with over 2 years of experience. Skilled in designing, developing, and testing performant production-ready web apps incorporating a range of modern technologies.",
+
+  // Additional
+  years_experience: "2+",
+  awards: "3",
+  project_fininhed: "15+",
+  stackoverflow_rating: "750+",
+
+  // Social Links
+  link_linkedin: "https://www.linkedin.com/in/ali-raza-4a3284164/",
+  link_stackoverflow: "https://stackoverflow.com/users/10115166/ali-raza",
+  link_github: "https://github.com/aliraza401",
+  link_instagram: "https://www.instagram.com/i_am_razaaa/",
+  link_twitter: "https://twitter.com/AliRaza00626002",
+
+  // Projects
+  projects: [
+    {
+      type: "Website",
+      name: "Dialboxx",
+      link: "https://www.dialboxx.pk/",
+      img: "https://res.cloudinary.com/markloops/image/upload/v1665224706/qjlwbgsox7nmq5yohyex.png",
+    },
+    {
+      type: "Website",
+      name: "Food Cart",
+      link: "https://foodcartsmanufacturing.com/",
+      img: "https://res.cloudinary.com/markloops/image/upload/v1665224728/phzmzmptazqirzl3zeq7.jpg",
+    },
+    {
+      type: "Website",
+      name: "Zip Homes",
+      link: "https://zip.homes/",
+      img: "https://res.cloudinary.com/markloops/image/upload/v1665224708/mtyor6icmnbzjzwd3c1v.png",
+    },
+    {
+      type: "Website",
+      name: "Vulcaz",
+      link: "https://getvulcaz.com/",
+      img: "https://res.cloudinary.com/markloops/image/upload/v1665224708/svgnchgtma0aarwxi9ln.png",
+    },
+    {
+      type: "Website",
+      name: "Son Of Leon",
+      link: "https://sonofleon.com/",
+      img: "https://res.cloudinary.com/markloops/image/upload/v1665224728/cnt7fek7ntxuptcebzlx.jpg",
+    },
+    {
+      type: "Website",
+      name: "Bunnye",
+      link: "https://bunnye.com/",
+      img: "https://res.cloudinary.com/markloops/image/upload/v1665224706/klnnuekepx4sgqbyb4iw.png",
+    },
+  ],
+
+  // Imgs
+  img_front:
+    "https://res.cloudinary.com/markloops/image/upload/v1665213704/as9e608pdblr6eygordw.jpg",
+  img_banner:
+    "https://res.cloudinary.com/markloops/image/upload/v1665213652/aw1pk2gx1a8cyt0r0ign.jpg",
+  img_background:
+    "https://res.cloudinary.com/markloops/image/upload/v1665224160/ge1s68dhee1jposn4e84.png",
+  img_projects:
+    "https://res.cloudinary.com/markloops/image/upload/v1665215100/wuazovoqmtm7ok476jat.jpg",
+  img_contact:
+    "https://res.cloudinary.com/markloops/image/upload/v1665215100/limzi6yyr5cvphxejcsk.jpg",
+  techStackLogos: [
+    "https://seeklogo.com/images/N/next-js-logo-7929BCD36F-seeklogo.com.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213651/yugkpe3mbb5z6nsffwzl.png",
+    "https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg",
+    "https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg",
+    "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665215017/qztbxxqvgx9tz5kyigdu.png",
+    "https://git-scm.com/images/logo@2x.png",
+    "https://react-query-v3.tanstack.com/_next/static/images/logo-7a7896631260eebffcb031765854375b.svg",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213651/nrq9tjoed65nlvuolan3.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213651/jpkdhjsqcuw6hdv8gi1o.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213651/inggqqphrwim35woriws.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213651/m7jqyjywfckgbavhaekz.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213651/vjsta3z66wrkwbxzkof9.png",
+    "https://res.cloudinary.com/markloops/image/upload/v1665213652/hc4kb6iu72tksv6vw8sb.png",
+  ],
+};
+
+export default data;
