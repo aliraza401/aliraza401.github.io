287c16ae873eb77a51b4c2c5caca3cf8fe3b4f36
Ghost <alirazamunir401@gmail.com>
2023-05-14 10:59:05 +0500

    build v__2

---
+++ _next/static/QPBB0xIvbn4QEXDqSWJ88/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/357-bbee361c63bf76f9.js","static/chunks/pages/index-ea1ac5df3b2d6ac8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/qrsMIud3m3k_fKl65f3iN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/357-bbee361c63bf76f9.js","static/chunks/pages/index-3d33ddbdd5b5f482.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
